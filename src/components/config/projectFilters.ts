d666599d8c8e0c9761b17fe0f3cba5443dbb62fa
 <dragonwanted95@gmail.com>
2026-08-16 16:23:24 -0600

    feat: prepare project filters for profile URLs

---
+++ src/components/config/projectFilters.ts
@@ -0,0 +1,38 @@
+export const PROFILE_TECHNOLOGIES: Record<string, string[]> = {
+    web: [
+        "html",
+        "css",
+        "javascript",
+        "react",
+        "nextjs",
+        "tailwind",
+        "bootstrap",
+        "node",
+    ],
+
+    dotnet: [
+        "csharp",
+        "aspnet",
+        "windows-forms",
+        "mvc",
+        "sql-server",
+    ],
+
+    fullstack: [
+        "html",
+        "css",
+        "javascript",
+        "react",
+        "nextjs",
+        "tailwind",
+        "bootstrap",
+        "node",
+        "csharp",
+        "aspnet",
+        "windows-forms",
+        "mvc",
+        "sql-server",
+        "mysql",
+        "postgresql",
+    ],
+};
